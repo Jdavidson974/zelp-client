66eb3b698692004da4c250cae821589385ea776c
Jdavidson <jd9743101@gmail.com>
2023-07-24 16:24:43 +0400

    auth init

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
 
 const routes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
+  {
+    path: "", component: AuthLayoutComponent,
+    children: [
+      { path: "", component: LoginComponent },
+      { path: "register", component: RegisterComponent },
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AuthLayoutComponent
   ],
   imports: [
     CommonModule,
